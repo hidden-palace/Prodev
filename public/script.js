f3a282ed824db4c2cd1dee5d811e4f350879e9f0
hidden-palace <ahmeddevs247@gmail.com>
2025-07-14 00:32:18 +0500

    Updated script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
-@@ .. @@
- // Load client-side modules
-+document.head.appendChild(Object.assign(document.createElement('script'), {
-+  src: '/client-error-handler.js',
-+  type: 'text/javascript'
-+}));
-+
-+document.head.appendChild(Object.assign(document.createElement('script'), {
-+  src: '/client-api.js', 
-+  type: 'text/javascript'
-+}));
-+
- document.head.appendChild(Object.assign(document.createElement('script'), {
+// Load client-side modules
+document.head.appendChild(Object.assign(document.createElement('script'), {
+  src: '/client-error-handler.js',
+  type: 'text/javascript'
+}));
+
+document.head.appendChild(Object.assign(document.createElement('script'), {
+  src: '/client-api.js', 
+  type: 'text/javascript'
+}));
+
+document.head.appendChild(Object.assign(document.createElement('script'), {
 // Load client-side modules
    src: '/error-integration.js',
+}));
